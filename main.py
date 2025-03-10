f82ca8678d14973229395228dcb518fb9a9e1959
Indian1999 <haszab99@gmail.com>
2025-03-10 16:27:10 +0100

    matrix random allocation

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+import random
+import matplotlib.pyplot as plt
+
 def generate_matrix(n, m):
     return [[0 for j in range(m)] for i in range(n)]
 
-matrix = generate_matrix(4, 6)
+matrix = generate_matrix(10, 10)
 for row in matrix:
     print(row)
+    
+for i in range(1, 101):
+    row = random.randrange(0, len(matrix))
+    col = random.randrange(0, len(matrix[row]))
+    matrix[row][col] = i
+    
+    
